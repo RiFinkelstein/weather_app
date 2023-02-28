96a870bbd2aa93c5ba43827c718f3d08e8a8cabb
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-28 18:04:00 +0200

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -10,3 +10,25 @@ parser.add_argument('--location', type=str, required=True, help='The location fo
 # Parse command line arguments
 args = parser.parse_args()
 location = args.location
+
+
+api_key = "c80b1a2d82ff9185cbe2051652745fb7"
+# Replace LOCATION with the name of the location you want weather data for
+location = "11598"
+
+# Make the API request
+url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}"
+response = requests.get(url)
+
+# Parse the JSON response
+data = response.json()
+
+# Extract the relevant weather information
+temperature = data["main"]["temp"]
+humidity = data["main"]["humidity"]
+wind_speed = data["wind"]["speed"]
+description = data["weather"][0]["description"]
+
+# Print the weather information
+print(f"The temperature in {location} is {temperature} K")
+print(f"The humidity in {location} is {humidity}%")
