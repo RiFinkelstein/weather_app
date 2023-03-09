aee887590c2b3896f9e0b88f8e1900e64153e296
riFinkelstein <rivkafinkelstein823@gmail.com>
2023-03-09 10:33:04 +0200

    added date time

--- weather_app.py
+++ weather_app.py
@@ -1,6 +1,8 @@
 import requests
 import argparse
 from tabulate import tabulate
+import datetime
+import pytz
 
 
 parser = argparse.ArgumentParser(
@@ -21,7 +23,7 @@ else:
 
 print(location)
 
-# ask what empurture unit
+# ask what tempurture unit
 if args.unit:
     unit = args.unit.upper()
 else:
@@ -37,6 +39,12 @@ response = requests.get(url)
 # Parse the JSON response
 data = response.json()
 
+# Define datetime_str
+datetime_str = ""
+
+# Define temperature
+temperature = 0
+
 # Check if "main" key exists in data dictionary
 if "main" in data:
     # Extract the relevant weather information
@@ -44,6 +52,7 @@ if "main" in data:
     humidity = data["main"]["humidity"]
     wind_speed = data["wind"]["speed"]
     description = data["weather"][0]["description"]
+    timezone_offset = data["timezone"]
 
     # Convert temperature to the specified unit
     if unit == "F":
@@ -56,8 +65,21 @@ if "main" in data:
         temperature = temperature_kelvin
         unit_symbol = "K"
 
+    # Get the timezone for the location
+    timezone = pytz.timezone(f'Etc/GMT{timezone_offset//3600:+d}')
+
+    # Get the current time in the timezone of the location
+    now = datetime.datetime.now(tz=timezone)
+
+    # Format the date and time string
+    datetime_str = now.strftime("%Y-%m-%d %H:%M:%S %Z%z")
+
     # Print the weather information
+    print(f"The date & time right now is: {datetime_str}")
     print(f"The temperature in {location} is {temperature:.1f} °C")
     print(f"The humidity in {location} is {humidity}%")
     print(f"The wind speed in {location} is {wind_speed} m/s")
     print(f"The weather description in {location} is {description}")
+
+else:
+    print("sorry there is an error")
